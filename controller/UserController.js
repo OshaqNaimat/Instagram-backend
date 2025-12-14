7dd4fcfe37ed85d59d83f3bc9d097901b37058d8
oshaq naimat <oshaqnaimat15@gmail.com>
2025-12-14 19:59:09 +0500

    till user login

--- controller/UserController.js
+++ controller/UserController.js
@@ -3,8 +3,22 @@ import { User } from "../models/UserModal.js";
 export const registeruser = async (req, res) => {
   const { mobile, password, fullName, username } = req.body;
   if (!mobile || !password || !fullName || !username) {
-    req.status(400);
+    res.status(400);
     throw new Error("Please fill all the fields");
+  }
+
+  // check email or mobile already exists
+  let checkEmail = await User.findOne({ mobile });
+  let checkUsername = await User.findOne({ username });
+
+  if (checkEmail) {
+    res.status(401);
+    throw new Error("Email already in use!");
+  }
+
+  if (checkUsername) {
+    res.status(401);
+    throw new Error("Username already in use!");
   }
 
   let newUser = await User.create({

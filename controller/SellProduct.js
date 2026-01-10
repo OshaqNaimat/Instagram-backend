69781d841754d2325467c28f178dfe48c4028dc6
Oshaq Naimat <oshaqnaimat15@gmail.com>
2026-01-10 23:29:19 +0500

    sell product controller

--- controller/SellProduct.js
+++ controller/SellProduct.js
@@ -1,9 +1,39 @@
-export const sellProduct = async (req,res)=>{
-    const {user_id} = req.params
-    const {productImage,productTitle,productDescription,productCategory,productPrice} = req.body
-    productCategory.create({
-        productImage,productTitle,productDescription,productCategory,productPrice,user_id
-    })
-}
+import Product from "../models/productModal.js";
 
-res.send()
+export const sellProduct = async (req, res) => {
+  try {
+    const { user_id } = req.params;
+    const {
+      productTitle,
+      productDescription,
+      productCategory,
+      productPrice,
+    } = req.body;
+
+    const productImage = req.file?.path; // multer image
+
+    if (!productImage) {
+      return res.status(400).json({ message: "Image is required" });
+    }
+
+    const product = await Product.create({
+      productImage,
+      productTitle,
+      productDescription,
+      productCategory,
+      productPrice,
+      user_id,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "Product listed successfully",
+      product,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};

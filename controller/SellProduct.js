3984c8a96ff38aa970f330d6e219e3d56ba0b305
Oshaq Naimat <oshaqnaimat15@gmail.com>
2026-01-10 23:04:10 +0500

    product modal

--- controller/SellProduct.js
+++ controller/SellProduct.js
@@ -1,4 +1,9 @@
 export const sellProduct = async (req,res)=>{
     const {user_id} = req.params
     const {productImage,productTitle,productDescription,productCategory,productPrice} = req.body
+    productCategory.create({
+        productImage,productTitle,productDescription,productCategory,productPrice,user_id
+    })
 }
+
+res.send()

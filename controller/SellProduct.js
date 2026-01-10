477559c93905175ceef1bc8dcd3fd747cb53ddda
Oshaq Naimat <oshaqnaimat15@gmail.com>
2026-01-10 18:26:47 +0500

    sell product

--- controller/SellProduct.js
+++ controller/SellProduct.js
@@ -1,38 +1,4 @@
-// controllers/productController.js
-const Product = require("../models/Product");
-
-exports.createProduct = async (req, res) => {
-  try {
-    const { user_id } = req.params;
-    const {
-      productTitle,
-      productPrice,
-      productDescription,
-    } = req.body;
-
-    const productImage = req.file ? req.file.path : "";
-
-    if (!productTitle || !productPrice || !productDescription || !productImage) {
-      return res.status(400).json({ message: "All fields are required" });
-    }
-
-    const product = await Product.create({
-      productImage,
-      productTitle,
-      productPrice,
-      productDescription,
-      user_id,
-    });
-
-    res.status(201).json({
-      success: true,
-      message: "Product created successfully",
-      product,
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      error: error.message,
-    });
-  }
-};
+export const sellProduct = async (req,res)=>{
+    const {user_id} = req.params
+    const {productImage,productTitle,productDescription,productCategory,productPrice} = req.body
+}
